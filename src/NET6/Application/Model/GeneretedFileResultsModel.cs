af6341380d85cd8aabe9bfed90d80aad7fbb065c
kazenetu <rakudafactory@gmail.com>
2022-07-04 23:45:17 +0900

    GeneretedFileResultsModelをrecord型に変更 #14

--- src/NET6/Application/Model/GeneretedFileResultsModel.cs
+++ src/NET6/Application/Model/GeneretedFileResultsModel.cs
@@ -5,20 +5,6 @@ namespace Application.Model
   /// <summary>
   /// 生成結果モデル
   /// </summary>
-  public class GeneretedFileResultsModel
-  {
-    /// <summary>
-    /// 生成結果メッセージ
-    /// </summary>
-    public ReadOnlyCollection<string> Messages { get; init; }
-
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    /// <param name="messages">生成結果メッセージ</param>
-    public GeneretedFileResultsModel(ReadOnlyCollection<string> messages)
-    {
-      Messages = messages;
-    }
-  }
+  /// <param name="Messages">生成結果メッセージ</param>
+  public record GeneretedFileResultsModel(ReadOnlyCollection<string> Messages);
 }
